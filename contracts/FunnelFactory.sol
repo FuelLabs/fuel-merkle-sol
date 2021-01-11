c20153909bb7665cc141f2b4a18343b96d52a070
John Adler <john.adler@protonmail.ch>
2021-01-10 19:06:51 -0500

    Add skeleton funnel factory.

---
+++ contracts/FunnelFactory.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity >=0.8.0 <0.9.0;
+pragma abicoder v2;
+
+/// @title Funnel deployer
+/// @notice Deploys funnel token contracts
+library FunnelFactory {
+
+}
