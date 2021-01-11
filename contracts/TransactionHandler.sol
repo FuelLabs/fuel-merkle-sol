defa5e2643c9840611b9a8c5531c93f6ae4b5960
John Adler <john.adler@protonmail.ch>
2021-01-10 22:57:52 -0500

    Add skeleton file for transaction manipulation library.

---
+++ contracts/TransactionHandler.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Apache-2.0
+
+pragma solidity >=0.8.0 <0.9.0;
+pragma abicoder v2;
+
+import "./types/TransactionLeaf.sol";
+import "./types/TransactionProof.sol";
+
+/// @title Transaction proof and leaf manipulation
+library TransactionHandler {
+
+}
