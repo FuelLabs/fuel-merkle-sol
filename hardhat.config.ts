6587261fd657743ea4d3ff987d8322ffdcb83ca9
Nick Dodson <thenickdodson@gmail.com>
2021-03-22 10:37:02 -0700

    Update hardhat.config.ts
    Co-authored-by: John Adler <adlerjohn@users.noreply.github.com>

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,7 +12,7 @@ const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
 const ROPSTEN_PRIVATE_KEY =
   process.env.ROPSTEN_PRIVATE_KEY ||
   "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
-const { ETHERSCAN_API_KEY } = process.env;
+const { ETHERSCAN_API_KEY } = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
