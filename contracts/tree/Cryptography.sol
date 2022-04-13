234297bf226b8bc0ddf1295566198de64a0763f1
John Adler <john.adler@protonmail.ch>
2022-04-13 13:55:42 -0400

    Add license identifier.

--- contracts/tree/Cryptography.sol
+++ contracts/tree/Cryptography.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.4;
 
 /// @notice This library abstracts the hashing function used for the merkle tree implementation
