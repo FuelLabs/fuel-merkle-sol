478e918ac71185d63e7b10011c41701e1348b8e9
Brandon Vrooman <brandon.vrooman@gmail.com>
2023-07-10 21:11:33 -0400

    Update proof tests

--- test/utils/proofTest.ts
+++ test/utils/proofTest.ts
@@ -3,6 +3,7 @@ import { EncodedValueInput } from './encodedValue';
 export default interface ProofTest {
 	name: string;
 	root: EncodedValueInput;
+	data: EncodedValueInput;
 	proof_set: EncodedValueInput[];
 	proof_index: number;
 	num_leaves: number;
